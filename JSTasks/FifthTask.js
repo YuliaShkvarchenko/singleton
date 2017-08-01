b92835e42d0ef1d4d484b743da2a92c09d6bf343
YuliaShkvarchenko <mirrayursa@gmail.com>
2017-08-01 11:42:58 +0300

    Developing addMethod

--- JSTasks/FifthTask.js
+++ JSTasks/FifthTask.js
@@ -5,7 +5,14 @@ function Library() {
     wholeLib.sort();
     console.log( wholeLib);
   };
-  this.addMethod = 
+  this.addMethod = function( methodName, code) {
+    Object.defineProperty( this, this.methodName, {
+      value: this.code
+    })
+  };
+  this.implement = function (this.methodName){
+    return this.methodName(bookName);
+  }
   
 }
 
@@ -15,3 +22,12 @@ var myLib1 = new Library();
 myLib.add("Stones");
 var myLib2 = new Library();
 myLib.add(1);
+
+myLib.addMethod(‘search’, function(bookName){
+  for ( var i = 0; i < wholeLib.length; i++){
+    if (wholeLib.[i] === bookName) 
+    return i;
+  }
+  return -1;
+
+});
