61b5a84d10dbbebd19ebd3272f05efa4ae66a3cb
YuliaShkvarchenko <mirrayursa@gmail.com>
2017-08-09 11:36:57 +0300

    Unfinished task

--- JSTasks/FifthTask.js
+++ JSTasks/FifthTask.js
@@ -1,9 +1,6 @@
 function Library() {
   var wholeLib = [];
-  /*
-	  var wholeLib = [];
-  
-  this.add = function(name) {
+    this.add = function(name) {
     wholeLib.push(name.toString());
     wholeLib.sort();
     console.log( wholeLib);
@@ -11,47 +8,25 @@ function Library() {
   this.getAllBooks = function(){
     return wholeLib;
   };
-  this.search = function(bookName){
-    var notPrivetLib = this.getAllBooks();
-    for ( var i = 0; i < notPrivetLib.length; i++){
-    if (notPrivetLib[i] === bookName) {
-    return notPrivetLib[i] ;
-    }
-    }
-  }
-}
-
-
-  
-  */
-  this.add = function(name) {
-    wholeLib.push(name.toString());
-    wholeLib.sort();
-    console.log( wholeLib);
+ this.addMethod = function(methodnName, func) {
+    this.name = func;
   };
-  this.addMethod = function( methodName, code) {
-    Object.defineProperty( this, this.methodName, {
-      value: this.code
-    })
-  };
-  this.implement = function (this.methodName){
-    return this.methodName(bookName);
-  }
   
 }
-
 var myLib = new Library();
 myLib.add("Ice and Fire");
 var myLib1 = new Library();
 myLib.add("Stones");
 var myLib2 = new Library();
 myLib.add(1);
-
-myLib.addMethod(‘search’, function(bookName){
-  for ( var i = 0; i < wholeLib.length; i++){
-    if (wholeLib.[i] === bookName) 
-    return i;
-  }
-  return -1;
-
+myLib.addMethod("search", function(book_name) {
+    var notPrivetLib = this.getAllBooks();
+    for ( var i = 0; i < notPrivetLib.length; i++){
+      if (notPrivetLib[i] === bookName){
+        console.log ("We have this book!");
+      }
+      else{
+        console.log("We can't help you.");
+      }
+    }
 });
