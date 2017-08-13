0b443aeaed3c2e11c57e903f5ec0eb04f8e27e52
YuliaShkvarchenko <mirrayursa@gmail.com>
2017-08-13 23:20:49 +0300

    The final version

--- JSTasks/FifthTask.js
+++ JSTasks/FifthTask.js
@@ -1,33 +1,36 @@
 function Library() {
   var wholeLib = [];
-    this.add = function(name) {
-    wholeLib.push(name.toString());
-    wholeLib.sort();
+  this.add = function(name) {
+    var stringName = name.toString();
+    if(!wholeLib.includes(stringName)) {
+      wholeLib.push(name.toString());
+      wholeLib.sort();
+    } else {
+      console.log("This book already exists.");
+    }
     console.log( wholeLib);
   };
   this.getAllBooks = function(){
     return wholeLib;
   };
  this.addMethod = function(methodName, func) {
-    this.methodName = func;
+    this[methodName] = func;
   };
   this.implement = function(methodName, arg){
-    this.methodName.call(this, arg);
+    this[methodName].call(this, arg);
   };
 }
 var myLib = new Library();
 myLib.add("Ice and Fire");
-var myLib1 = new Library();
 myLib.add("Stones");
-var myLib2 = new Library();
+myLib.add(1);
 myLib.add(1);
 myLib.addMethod("search", function(bookName) {
-     var notPrivetLib = this.getAllBooks();
-   if (notPrivetLib.indexOf(bookName)>=0){
-     console.log("We have this book!");
-   }
-   else{
-     console.log("Sorry. We can't help you.");
-   }
+  var publicLib = this.getAllBooks();
+  if (publicLib.includes(bookName)){
+    console.log("We have this book!");
+  } else{
+    console.log("Sorry. We can't help you.");
+  }
 });
 myLib.implement("search","Ice and Fire");
