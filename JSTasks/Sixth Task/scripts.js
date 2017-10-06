52103a4618c3ddf85a53a813fc6c38ec917550d8
YuliaShkvarchenko <mirrayursa@gmail.com>
2017-10-06 15:49:11 +0300

    methoa SubmitButton was added

---
+++ JSTasks/Sixth Task/scripts.js
@@ -0,0 +1,37 @@
+function createForm(){
+
+    var newForm = document.createElement('form');
+	document.body.appendChild(newForm);
+	newForm.style.marginBottom = "10px";
+
+
+	var inputUrl = document.createElement('input');
+	newForm.appendChild(inputUrl);
+	newForm.firstChild.focus();
+	inputUrl.setAttribute("type","url");
+	inputUrl.setAttribute("required","required");
+	inputUrl.style.marginBottom = "10px";
+
+	var separator = document.createElement('br');
+	newForm.appendChild(separator);
+
+    var submitButton = document.createElement('input');
+	submitButton.setAttribute("type","submit");
+	submitButton.setAttribute("value","Check");
+	newForm.appendChild(submitButton);
+
+	submitButton.onclick = function(){
+		var rightUrl = "https://www.google.com.ua";
+		switch(inputUrl.value) {
+			case rightUrl:
+
+				newForm = document.createElement('iframe');
+				document.body.appendChild(newForm);
+				newForm.setAttribute("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtnPWrYyMA2uCg9Kl28kLRR7R20p0L4ogjkDNhvCeCTYSPtPyTfg");
+				break;
+			case !rightUrl:
+				alert("Вы ввели неправильный url. Попытайтесь ещё.");
+		
+		}
+	};
+};
